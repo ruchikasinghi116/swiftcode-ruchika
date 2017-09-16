4ed816037c71c9bbf79ea01725573a921d13f93f
ruchikasinghi116 <singhi.ruchi31@gmail.com>
2017-09-16 11:44:33 +0000

    done

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -7,27 +7,9 @@ var app=angular.module('chatApp',['ngMaterial'])
 app.controller('chatController',function($scope,$sce){
 	$scope.messages=[
 
-	// {   sender:"BOT",
-	// 	text:"hey! who are you?",
-	// 	time:"1:25pm"		
-	// },
+		];
 
-	// {   sender:"USER",
-	// 	text:"hi!I am Ruchika",
-	// 	time:"1:24:"
-	// },
-	// {	   sender:"BOT",
-	// 	text:"wh0 is nishanth? ",
-	// 	time:"1:23pm"
-	// },
-	// {   sender:"USER",
-	// 	text:"An idiot!!",
-	// 	time:"1:23pm"
-	// }
-
-	];
-
-var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+var  exampleSocket =  new  WebSocket("wss://swiftcode-ruchika.herokuapp.com/chatSocket");
     exampleSocket.onmessage  =   function(event) {
     var jsonData = JSON.parse(event.data); 
     jsonData.time = new Date().toLocaleTimeString();
